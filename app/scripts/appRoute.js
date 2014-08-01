55c35e4ddc6b10e5d6fdf38cfb31b7b3b25f0fa0
shibatako <koki305@gmail.com>
2014-08-01 23:06:43 +0900

    add ckeditor

--- app/scripts/appRoute.js
+++ app/scripts/appRoute.js
@@ -12,15 +12,7 @@ jQuery(function() {
     'use strict';
     var app = Sammy('#SimpleWebDevTool', function(app) {
         // define a 'get' route that will be triggered at '#/path'
-        app.get('#/path', function() {
-            $('#template').html(_.template(simpleWebDevTool.util.render('template1')));
-            simpleWebDevTool.controller.pathController().init();
-        });
 
-        app.get('#/path2', function() {
-            $('#template').html(_.template(simpleWebDevTool.util.render('template2')));
-            simpleWebDevTool.controller.path2Controller().init();
-        });
         app.get('#/vue/:id', function(context) {
             console.log('access to #/vue');
             $('#template').html(_.template(simpleWebDevTool.util.render('vueTemplate')));
@@ -30,10 +22,6 @@ jQuery(function() {
             console.log('access to #/jquery');
             $('#template').html(_.template(simpleWebDevTool.util.render('jqueryTemplate')));
             simpleWebDevTool.controller.jqueryController(context.params.id).load();
-        });
-        app.get('#/path2/:id', function(context) {
-            // this context is a Sammy.EventContext
-            console.log(context.params.id);
         });
 
     });
